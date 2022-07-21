828e3b8e22edcab1db27b48ddfd71ac2455d711f
leilani <lherna05@gmail.com>
2022-07-20 23:43:13 -0700

    code updated & index html created

--- data/background.js
+++ data/background.js
@@ -1,15 +1,37 @@
-let color = '#3aa757';
 
-//create function assigned to an array of preset party color 
-//LINK TO COLOR PALLETTE: https://www.shutterstock.com/blog/neon-color-palettes
-const colors = ['#08F7FE', '#09FBD3', '#FE53BB', '#F5D300'];
-
-//create function that changes background by iterating through color array
-
+//invoke addListening on chrome's runtime.onInstalled function (will indicate what to listen for and what functionality to implement)
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+
+// FEATURE 1: Background Color Change
+  //Description: once the partyMode extension is turned on, the app will change the background color. 
+    //MVP: Change to 1 specified color
+    //Stretch: Have the background continuously alternate colors
+
+///testing 1 color: 
+let color = '#3aa757';
+
+//create function assigned to an array of preset party colors 
+  //LINK TO COLOR PALLETTE: https://www.shutterstock.com/blog/neon-color-palettes
+const colors = ['#08F7FE', '#09FBD3', '#FE53BB', '#F5D300'];
+
+//create function that iterates through 
+function randomColor() {
+  return '#'+ ('000000' + (Math.random()*0xFFFFFF<<0).toString(16)).slice(-6)
+}
+
+function setColor(){
+  colors.forEach( el => {
+    document.getElementById('div').style.backgroundColor = el;
+    setTimeout(setColor, 2000);
+  });
+ 
+}
+
+setColor();
 
 
 
