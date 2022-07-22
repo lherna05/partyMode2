6b2781c55b77d818521b7fa25eea0ce14944c23b
leilani <lherna05@gmail.com>
2022-07-21 17:42:03 -0700

    clicks

--- background2.js
+++ background2.js
@@ -4,9 +4,12 @@ const changePage = () =>{
 const parent = root.parentNode;
 root.parentNode.removeChild(root);
 let div = document.createElement('div');
-div.innerHTML = "🎉🎉🎉🥳    HI   🥳🎉🎉🎉";
-
+div.innerHTML = "🎉🎉🎉🥳    YOU FIGURED IT OUT! NOW IT'S TIME TO PARTY!   🥳🎉🎉🎉";
 div.style.backgroundColor = '#08F7FE';
+// div.style.fontSize = 'x-large';
+// div.style.fontFamily = 'Impact,Charcoal,sans-serif';
+// div.style.color = '#FE53BB00';
+
 div.style.left = '50%';
 div.style.left = '50%';
 div.style.height = "1000px";
@@ -14,7 +17,7 @@ div.style.width = '100%';
 parent.appendChild(div);
 }
 
-chrome.action.setIcon({ "path: /assets/32party-icon.gif" });
+// chrome.action.setIcon({ path: "/assets/16party.jpeg" });
 
 root.addEventListener('click',changePage);
 
